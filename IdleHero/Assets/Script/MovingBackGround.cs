8ac2cf58d03065d7793a4b7bf1051a63ecad254a
RyouMisakl <33336762+RyouMisakl@users.noreply.github.com>
2019-08-25 14:20:48 +0800

    background moving now need to see what size the camera needs to be

---
+++ IdleHero/Assets/Script/MovingBackGround.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackGround : MonoBehaviour
+{
+    public GameObject xlocation;
+    public float maxlocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x > maxlocation)
+        {
+            this.transform.position = new Vector3(this.transform.position.x - 0.01f, this.transform.position.y, this.transform.position.z);
+        }
+
+        else
+        {
+            this.transform.position = new Vector3(xlocation.transform.position.x - 0.01f, this.transform.position.y, this.transform.position.z);
+        }
+    }
+}
